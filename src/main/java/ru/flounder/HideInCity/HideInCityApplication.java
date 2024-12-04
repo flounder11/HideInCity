aff61ea93770f981a27ecb0643d470cd472c4672
Fodi <flounder06@mail.ru>
2024-12-04 23:23:48 +0300

    add project

---
+++ src/main/java/ru/flounder/HideInCity/HideInCityApplication.java
@@ -0,0 +1,13 @@
+package ru.flounder.HideInCity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HideInCityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HideInCityApplication.class, args);
+	}
+
+}
